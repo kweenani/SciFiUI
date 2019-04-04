d2aafec5ad23a21ee999140540f06b098dcd8ed7
kween <D12125516@mydit.ie>
2019-04-04 11:54:48 +0100

    change size of the circle

--- java/src/ie/tudublin/Radar.java
+++ java/src/ie/tudublin/Radar.java
@@ -15,13 +15,13 @@ public class Radar{
     public void render(){
         ui. stroke(255);
         ui.noFill();
+        ui.ellipse(400,400,500,500); // x,y, width,height
+        ui.ellipse(400,400,450,450);
         ui.ellipse(400,400,400,400);
         ui.ellipse(400,400,350,350);
         ui.ellipse(400,400,300,300);
         ui.ellipse(400,400,250,250);
         ui.ellipse(400,400,200,200);
-        ui.ellipse(400,400,150,150);
-        ui.ellipse(400,400,100,100);
         ui.line(400,200,400,600);
         ui.line(200,400,600,400);
     }
