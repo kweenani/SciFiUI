c1e6d29b259ec3fa23a01da02766ab4fd5ace72b
kween <D12125516@mydit.ie>
2019-05-04 02:43:24 +0100

    cloud class created

---
+++ java/src/ie/tudublin/Cloud.java
@@ -0,0 +1,41 @@
+
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class Cloud {
+
+    PApplet ui; 
+    private float x;
+    private float y;
+    private float xspeed;
+    private float cdiameter = 70;
+    
+    public Cloud(PApplet ui){
+        this.ui = ui;
+        this.x = ui.random(-200, -100);
+        this.y  = ui.random(ui.height);
+        this.xspeed = ui.random(1, 2);;
+      }
+
+
+    public void drift() {
+        x = x + xspeed;
+            
+        
+        if (x > ui.width) {
+        x = ui.random(-200, -100);// this loop makes the returm to the top again when it falls
+        xspeed = ui.random(1, 2); 
+        }
+    }
+    
+    public void show() {
+        ui.noStroke();
+        ui.fill(138, 43, 226);
+        
+        ui.ellipse(x, y, cdiameter, cdiameter);
+        ui.ellipse(x - (cdiameter/2) , y , cdiameter, cdiameter);
+        ui.ellipse(x + (cdiameter/2) , y , cdiameter, cdiameter);
+        ui.ellipse(x , y - (cdiameter/2) , cdiameter, cdiameter);
+    }
+  }
